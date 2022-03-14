b4f7b1d675176a7d68103c1c2d61dae64993a5d3
sugiyama <sgym.snk@gmail.com>
2022-03-14 09:22:55 +0900

    express

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
+import express from 'express';
+import cors from 'cors';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { addResolversToSchema } from '@graphql-tools/schema';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { join } from 'path';
 
 import { resolvers } from './resolvers/index';
 // import { posts, users } from './data';
 
-const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
-  loaders: [new GraphQLFileLoader()],
-});
+const app = express();
+app.use(cors());
 
-const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
-const server = new ApolloServer({
-  schema: schemaWithResolvers,
-  // context: ({ req }) => {
-  //   return { user: req.headers.authorization };
-  // },
-});
+const initGraphqlServer = async () => {
+  const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+  });
 
-server.listen().then(({ url }) => {
-  console.info(`Server ready at ${url}`);
+  const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
+  const server = new ApolloServer({
+    schema: schemaWithResolvers,
+    // context: ({ req }) => {
+    //   return { user: req.headers.authorization };
+    // },
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  return server;
+};
+
+initGraphqlServer().then((server) => {
+  app.listen({ port: 4000 }, () => {
+    console.info(
+      `express server start http://localhost:4000${server.graphqlPath}`
+    );
+  });
 });
